5b30eceb752c2396b7ddd3957292abecaf09dc90
Ryan <112456301+ryanpchc@users.noreply.github.com>
2026-06-09 15:57:14 -0700

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record = []
+
+        for op in operations:
+            try:
+                op = int(op)
+                record.append(op)
+            except ValueError:
+                if op == "+":
+                    last_two_sum = record[-1] + record[-2]
+                    record.append(last_two_sum)
+                elif op == "D":
+                    double_previous_score = 2 * record[-1]
+                    record.append(double_previous_score)
+                else:
+                    record.remove(record[-1])
+        
+        record_sum = sum(n for n in record)
+        return record_sum
