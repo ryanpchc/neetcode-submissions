7eff12f86d22f9404f3b0e3c8e59e875adcc799c
Ryan <112456301+ryanpchc@users.noreply.github.com>
2026-06-10 16:08:41 -0700

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        queue = deque(students)
+        sandwich_idx = 0
+        no_match_count = 0
+
+        while queue and no_match_count < len(queue):
+            if queue[0] == sandwiches[sandwich_idx]:
+                queue.popleft()
+                sandwich_idx += 1
+                no_match_count = 0
+            else:
+                queue.rotate(-1)
+                no_match_count += 1
+
+        return len(queue)
